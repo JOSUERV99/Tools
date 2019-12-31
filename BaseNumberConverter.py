24cb4c4825aec40dc0e9c9717805cd0363466d0e
josuerv99 <josuerojasvega@gmail.com>
2019-12-31 05:55:03 -0600

    BaseNumberConverter: dec -> to hex ready and bug (dec->bin) solved

--- BaseNumberConverter.py
+++ BaseNumberConverter.py
@@ -10,25 +10,49 @@ def binToDec(binario):
     return decNumber
 
 def decToBin(decimal):
-    binario = "" # TODO: conversion de numeros negativos, and bug
-    if decimal != 0:        
-        for i in range(ceil(log(decimal))+1, 0, -1):
-            print(decimal, end="\t")
-            if 2**i <= decimal:
-                binario += "1"
-                decimal -= 2**i
-            else:
-                binario += "0"
-    else:
+
+    def calculateSpaces(decimal, counter):
+        if decimal > 2**counter:
+            return calculateSpaces(decimal-2**counter, counter+1)
+        else:
+            return counter
+
+    def bin(dec, spaces):
+        if spaces < 0:
+            return ""
+
+        if 2**spaces <= dec:
+            return "1" + bin(dec-2**spaces, spaces-1)
+        elif 2**spaces > dec:
+            return "0" + bin(dec, spaces-1)
+    
+    spaces = calculateSpaces(decimal, 0)
+    if decimal == 0:
         return "0"
-    return binario
+    else:
+        return bin(decimal, spaces)
 
 def decToHex(decimal):
-    hexa = "" # TODO: all xD
+    hexa = ""
     binario = decToBin(decimal)[::-1]
-    print(binario)
-    aux = bandera = 0
-    for i in range(len(binario), 4):
-        print(aux)
-        aux = 0
+    while (len(binario)%4 != 0):
+        binario+="0"
+    currentDigit = 0
+    print(binario, end="\t")
+    #binario = binario[::-1]
+    counter = 0
+    for i in range(int(len(binario)/4)):
+        for j in range(4):    
+            currentDigit += int(binario[counter]) * 2**(j)
+            counter+=1
+        if currentDigit < 10:
+            hexa += str(currentDigit)
+        else:
+            hexa += str(chr(currentDigit+87)).upper()
+        currentDigit = 0
+    return hexa[::-1]
 
+print("--HEX--")
+numeros = [0, 15, 16, 1231, 11, 12]
+for i in numeros:
+    print(i, "->", decToHex(i))
