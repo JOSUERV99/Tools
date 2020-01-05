4dc59b01ceae467a256de970e71e62e77c8627db
josuerv99 <josuerojasvega@gmail.com>
2020-01-05 00:17:44 -0600

    BaseNumberConverter: init of the tkinter gui

--- BaseNumberConverter.py
+++ BaseNumberConverter.py
@@ -1,109 +1,41 @@
 """
     Converting any number of any base to another base with a tkinter gui
     @author: Josue Rojas
-    @since: 03/01/2020
-
-        BIN -> HEX *
-        BIN -> OCT
-        BIN -> DEC *
-        HEX -> BIN
-        HEX -> OCT
-        HEX -> DEC x
-        OCT -> BIN
-        OCT -> HEX
-        OCT -> DEC *
-        DEC -> BIN *
-        DEC -> HEX *
-        DEC -> OCT
+    @since: 05/01/2020
 """
 
 from math import log, ceil
-import tkinter
-validChars = "0123456789ABCDEF" #TODO: modifiy allToDec for hex numbers
-headers = ["BIN", "HEX", "OCT", "DEC"]
+import tkinter as tk
 
-def allToDec(number, base=10):
-    decimal = 0
-    number = number[::-1]
-    for i in range(len(number)):
-        decimal += int(number[i]) * base**i
-    return decimal
+style = ["DEF","ABC", "789", "456", "123", "0RC"]
 
-def allToBin(number, range=3):
-    def calculateSpaces(decimal, counter):
-        if decimal > 2**counter:
-            return calculateSpaces(decimal-2**counter, counter+1)
-        else:
-            return counter
-    
-    if ( range==4 ):
-        #hex
-        pass
-    else:
-        pass
+def initWindow():
+    global style
+    w = tk.Tk()
+    w.title("Ventana")
+    w.geometry('500x500')
+    w.configure(background='black')
 
-def binToOct(binario):
-    octal = ""
-    for i in range(len(binario)):
-        if binario[i] == 1:
-            octal += str( 8**i )
-    return octal[::-1]
+    #components
+    for r in range(6):
+        for c in range(3):
+            b = tk.Button(w, text=style[r][c], bg='red', fg='yellow', font='arial')
+            b.grid(row = r, column = c, pady=5, padx=5)
+    return w
 
-def binToHex(binario):
-    return decToHex(allToDec(binario, base=2))
+initWindow().mainloop()
 
-def decToBin(decimal):
+# bin, hex, oct for dec to any number
+# dec to bin, hex and oct
+# n = 10
+# n = '0xf101'
+# print(bin(n))
+# print(hex(n))
+# print(oct(n))
+# print(int(bin(n), 2))
+# print(int(hex(n), 16))
+# print(int(oct(n), 8))
+#hexToOct return oct(int(hex, 16))
+#octToHex return hex(int(oct, 8))
 
-    def calculateSpaces(decimal, counter):
-        if decimal > 2**counter:
-            return calculateSpaces(decimal-2**counter, counter+1)
-        else:
-            return counter
-
-    def bin(dec, spaces):
-        if spaces < 0:
-            return ""
-        if 2**spaces <= dec:
-            return "1" + bin(dec-2**spaces, spaces-1)
-        elif 2**spaces > dec:
-            return "0" + bin(dec, spaces-1)
-    
-    spaces = calculateSpaces(decimal, 0)
-    if decimal == 0:
-        return "0000"
-    else:
-        binario = bin(decimal, spaces)[::-1]
-        while(len(binario)%4!=0):
-            binario+="0"
-        return binario[::-1]
-
-def decToHex(decimal):
-    hexa = ""
-    binario = decToBin(decimal)[::-1]
-    currentDigit = 0
-    #print(binario, end="\t")
-    #binario = binario[::-1]
-    counter = 0
-    for i in range(int(len(binario)/4)):
-        for j in range(4):    
-            currentDigit += int(binario[counter]) * 2**(j)
-            counter+=1
-        if currentDigit < 10:
-            hexa += str(currentDigit)
-        else:
-            hexa += str(chr(currentDigit+87)).upper()
-        currentDigit = 0
-    return hexa[::-1]
-
-numeros = [0, 15, 16, 1231, 31, 1, 3]
-bina = ["1101", "01010001", "10100001", "11110001", "10110111", "00000001", "11110001"]
-for i in range(len(numeros)):
-    print("DEC->BIN", numeros[i], "= "+decToBin(numeros[i]))
-    print("BIN->HEX", bina[i], "= "+binToHex(bina[i]))
-    print("DEC->HEX", numeros[i], "= "+decToHex(allToDec(decToBin(numeros[i]), base=2)))
-    print("BIN->DEC", bina[i], "= "+str(allToDec(bina[i], base=2)))
-    print("     BIN->OCT", bina[i], "= "+binToOct(bina[i]))
-
-decTest =  ["1010", 2, "F"]
-print("123", allToDec("123", 8))
 
