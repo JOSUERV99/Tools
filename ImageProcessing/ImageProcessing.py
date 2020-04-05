627b1ec7e8b3db64552bcd6a4b044ed9d92b6e3d
josuerv99 <josuerojasvega@gmail.com>
2020-04-05 17:04:49 -0600

    ImageProcessing: convert a image to black and white

---
+++ ImageProcessing/ImageProcessing.py
@@ -0,0 +1,36 @@
+""" 
+    Objective:
+    Convert a image into a black and white image
+    @author: josuerv99
+"""
+
+import cv2 # pip install opencv-python
+
+image_path = 'Image1.png' # (R, G, B, A)
+black_and_white_image_path = 'b&w.png'
+
+def processingImage(imagePath):  
+    image = cv2.imread(imagePath)
+    adjustColors(image)
+    return image
+
+def adjustColors(image):
+    width, height, pixels_length = image.shape[0], image.shape[1], image.shape[2]
+    for x in range(width):
+        for y in range(height):
+            image[x,y] = [ sum(image[x,y])/pixels_length for z in range(pixels_length) ]
+    return image
+
+def showImage(image, title='img1'):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def saveImage(image, filename='img_b&w.png'):
+    cv2.imwrite(filename, image)
+    print("Saved!")
+
+print('Processing image...')
+black_and_white_image = processingImage(image_path)
+showImage(black_and_white_image, title='Black and White Image')
+saveImage(black_and_white_image, filename=black_and_white_image_path )
